a489f15dddb810078efc0b8621b846ab050cb33f
brungmail <bruh@gmail.com>
2024-01-01 18:19:25 -0500

    fixed dotenv import

--- server.js
+++ server.js
@@ -11,7 +11,6 @@ import path from 'path'
 import { fileURLToPath } from 'url';
 import { dirname } from 'path';
 import clerkClient from '@clerk/clerk-sdk-node';
-import 'dotenv/config';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
